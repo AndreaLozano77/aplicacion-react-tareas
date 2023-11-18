a0236f6862b037fb65a7aa99e181f3a11e63822f
andrea lozano <lozano.andrea.1977@gmail.com>
2023-11-18 14:43:41 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import freeCodeCampLogo from './imagenes/logo-freecodecamp.jpg'
+import ListaDeTareas from './componentes/ListaDeTareas';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Contenedor principal
+    <div className="aplicacion-tareas">
+      <div className='freecodecamp-logo-contenedor'>
+       <img src={freeCodeCampLogo} className='freecodecamp-logo' />
+      </div>
+      <div className='tareas-lista-principal'>
+       <h1>Mis Tareas</h1>
+       <ListaDeTareas />
+      </div>
     </div>
   );
 }
